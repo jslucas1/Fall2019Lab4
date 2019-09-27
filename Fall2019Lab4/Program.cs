2364c388349aef60778aaa4c846077b0f2622aa2
Jeff Lucas <jslucas1@msn.com>
2019-09-27 07:21:55 -0500

    Add project files.

---
+++ Fall2019Lab4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2019Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize variables
+
+            /* Add program flow of control
+             * for this application that is likely a while loop
+             * that makes a call to a method to display the menu and return the users choice
+             * The flow of control loop would use the result from the menu choice
+             * to call the proper method or exit the program.
+             * 
+             * That means at a minimun you will have a method for the menu
+             * a method for the triangle and a method for the numbers table
+             */
+
+
+        }
+    }
+}
